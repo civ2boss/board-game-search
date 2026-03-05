d67817a8b34cf931e6374beeb2dcfca26aa388de
Robin Ting <civ2boss@gmail.com>
2026-03-05 10:45:26 -0500

    Capture page leave

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,12 @@ import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 import posthog from 'posthog-js';
-import { PostHogProvider } from '@posthog/react'
+import { PostHogProvider } from '@posthog/react';
 
 posthog.init(import.meta.env.VITE_PUBLIC_POSTHOG_TOKEN, {
   api_host: import.meta.env.VITE_PUBLIC_POSTHOG_HOST,
   defaults: '2026-01-30',
+  capture_pageleave: true,
 });
 
 createRoot(document.getElementById('root')!).render(
@@ -15,5 +16,5 @@ createRoot(document.getElementById('root')!).render(
     <PostHogProvider client={posthog}>
       <App />
     </PostHogProvider>
-  </StrictMode>
+  </StrictMode>,
 );
